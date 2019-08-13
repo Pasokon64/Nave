34872c475d1c0d7ae9bfb84b0efabb1e57d9835c
Manoel Almeida <tmanoel64x@gmail.com>
2019-08-12 22:30:11 -0300

    controle da nave

--- public_html/script.js
+++ public_html/script.js
@@ -10,9 +10,41 @@ var ctx = canvas.getContext("2d");
 canvas.width = WIDTH;
 canvas.height = HEIGHT;
 
+var rightPressed = false;
+var leftPressed = false;
+
 var ship = new Ship();
 ship.ship_x = CENTER_WIDTH;
 ship.ship_y = CENTER_HEIGHT;
+
+document.addEventListener('keydown', KeyDownHandler, false);
+document.addEventListener('keyup', KeyUpHandler, false);
+
+function KeyDownHandler(e) {
+    
+    if(e.key == 'Right' || e.key == 'ArrowRight') {
+
+        rightPressed = true;
+    }
+
+    if(e.key == 'Left' || e.key == 'ArrowLeft') {
+
+        leftPressed = true;
+    }
+}
+
+function KeyUpHandler(e) {
+    
+    if(e.key == 'Right' || e.key == 'ArrowRight') {
+
+        rightPressed = false;
+    }
+
+    if(e.key == 'Left' || e.key == 'ArrowLeft') {
+
+        leftPressed = false;
+    }
+}
 
 function draw() {
    
@@ -20,7 +52,16 @@ function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     
     ship.draw(ctx);
-    ship.turn(2);
+
+    if(rightPressed) {
+    
+        ship.turn(-2);
+    }
+
+    if(leftPressed) {
+    
+        ship.turn(2);
+    }
 }
 
 setInterval(draw, 10);
