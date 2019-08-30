2f1f76816dabf042abd37b6013ed4ac75532adb1
= <=>
2019-08-30 16:42:50 -0300

    Calculando pontos reais do asteroid

--- public_html/modules/asteroid.js
+++ public_html/modules/asteroid.js
@@ -7,22 +7,32 @@ function Asteroid() {
     this.angle = 0;
     this.move_angle = Math.floor(Math.random() * 360);
     this.velocity = 0.3;
+    this.hit = false;
     
     this.points = [ [180, 25], [270, 25], [0, 25], [90, 25] ];
+    this.real_points = [];
+    this.qtd_real_points = 0;
     
     this.draw = function(ctx) {
         
+        this.qtd_real_points = 0;
+
         var current_x = this.x + Math.sin((this.angle + 45) * Math.PI / 180) * (25 / Math.sin(45 * Math.PI / 180) / 2);
         var current_y = this.y + Math.cos((this.angle + 45) * Math.PI / 180) * (25 / Math.sin(45 * Math.PI / 180) / 2);
         
         ctx.beginPath();
         ctx.moveTo(current_x, current_y);
         
+        this.real_points[0] = [current_x, current_y];
+
         for(var i = 0; i < this.points.length; i++) {
             
             current_x += Math.sin((this.angle + this.points[i][0]) * Math.PI / 180) * this.points[i][1];
             current_y += Math.cos((this.angle + this.points[i][0]) * Math.PI / 180) * this.points[i][1];
             ctx.lineTo(current_x, current_y);
+
+            this.real_points[i + 1] = [current_x, current_y];
+            this.qtd_real_points = i + 1;
         }
         
         ctx.strokeStyle = 'rgb(255, 255, 255)';
