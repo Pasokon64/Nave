a77c3ba5f1275e740c0def1f1c81fafea2a311c4
Manoel Almeida <tmanoel64x@gmail.com>
2019-09-01 02:02:55 -0300

    Comentando script.js

--- public_html/script.js
+++ public_html/script.js
@@ -1,10 +1,15 @@
+// dimensões do canvas
 const WIDTH = 500;
 const HEIGHT = 500;
 
+// centro do canvas
 const CENTER_WIDTH = WIDTH / 2;
 const CENTER_HEIGHT = HEIGHT / 2;
 
+// número de tiros máximos permitidos simultaneamente
 const MAX_SHOOTS = 25;
+
+// tempo de espera mínimo entre um tiro e outro
 const SHOOT_DELAY = 100;
 
 var canvas = document.getElementById("canvas");
@@ -13,24 +18,34 @@ var ctx = canvas.getContext("2d");
 canvas.width = WIDTH;
 canvas.height = HEIGHT;
 
+// armazena quando a última foi disparada para o 
+// calculo de delay entre uma bala e outra
 var lastBullet = new Date(); 
 
+// variáveis que guardam quais botões foram precionados
 var rightPressed = false;
 var leftPressed = false;
 var upPressed = false;
 var downPressed = false;
 var spacePressed = false;
 
+// criando e posicionando um objeto nave
 var ship = new Ship();
 ship.x = CENTER_WIDTH;
 ship.y = CENTER_HEIGHT;
 
+// criando um novo objeto asteroide
 var asteroid = new Asteroid();
 
+// vetor que guarda as balas disparadas
 var shoots = [];
+
+// número de balas presentes atualmente na tela
 var num_shoots = 0;
 
+// adicionando eventos para quando algum botão for precionando
 document.addEventListener('keydown', KeyDownHandler, false);
+// adicionando eventos para quando algum botão for solto
 document.addEventListener('keyup', KeyUpHandler, false);
 
 function KeyDownHandler(e) {
@@ -89,22 +104,32 @@ function KeyUpHandler(e) {
     }
 }
 
+/**
+ * É chamada a cada 1 milisegundo. Essa função é responsável
+ * pelas atualizações na gameplay do jogo.
+ */
 function update() {
    
-    //limpando a tela
+    // limpa a tela para o desenho do próximo frame
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     
+    // desenha o objeto nave na tela
     ship.draw(ctx);
 
     if(asteroid.hit === false)
         asteroid.draw(ctx);
-    
+
+    // loop que itera todos os tiros presentes na tela
     for(var i = 0; i < MAX_SHOOTS; i++) {
         
         if(shoots[i] !== undefined) {
 
-            if(shoots[i].x < WIDTH && shoots[i].x > 0 && shoots[i].y < HEIGHT && shoots[i].y > 0)
+            // caso o tiro ainda esteja dentro dos limites do canvas
+            if(shoots[i].x < WIDTH && shoots[i].x > 0 && shoots[i].y < HEIGHT && shoots[i].y > 0) {
+
+                //desenha o objeto tiro na tela
                 shoots[i].draw(ctx);
+            }
 
             if(collision_shoot_asteroid(shoots[i], asteroid)) {
 
@@ -151,27 +176,16 @@ function update() {
     }
 }
 
+/**
+ * Calcula se bala e asteroid estão colidindo entre si.
+ * 
+ * @param {Shoot} shoot bala na qual será verificada a colisão.
+ * @param {Asteroid} asteroid asteroide no qual será verificado a colisão
+ * @returns {boolean} Se bala e asteroid estão colidindo.
+ */
 function collision_shoot_asteroid(shoot, asteroid) {
 
     //TODO: calcular colisão entre uma bala e um asteroid
-    for(let i = 0; i < (asteroid.real_points.length - 1); i++) {
-
-        let x1 = Number((asteroid.real_points[i][0]).toFixed(1));
-        let x2 = Number((asteroid.real_points[i + 1][0]).toFixed(1));
-
-        let y1 = Number((asteroid.real_points[i][1]).toFixed(1));
-        let y2 = Number((asteroid.real_points[i + 1][1]).toFixed(1));
-
-        //calculando o coeficiente angular da reta
-        let ang_coefficient = Number(((y2 - y1) / (x2 - x1)).toFixed(1));
-
-        //se o tiro pertencer a reta do asteroid
-        if((shoot.y.toFixed(1) - y1).toFixed(2) === (ang_coefficient * (shoot.x.toFixed(1) - x1)).toFixed(2)) {
-
-            return true;
-        }
-    }
-
     return false;
 }
 
