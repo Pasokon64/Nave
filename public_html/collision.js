0cf3460e522b6a07406dce1f9284acfc78ed58ad
Manoel Almeida <tmanoel64x@gmail.com>
2019-09-08 00:17:49 -0300

    Colisão entre nave e asteroide finalizada

--- public_html/collision.js
+++ public_html/collision.js
@@ -23,12 +23,55 @@ class Collision {
         return distance <= r;
     }
 
+    static linePoint(sX, sY, eX, eY, pX, pY) {
+
+        let dist1 = Physics.distance(pX, pY, sX, sY);
+        let dist2 = Physics.distance(pX, pY, eX, eY);
+
+        let line_len = Physics.distance(sX, sY, eX, eY);
+
+        if(dist1 + dist2 === line_len) {
+
+            return true;
+        }
+
+        return false;
+    }
+
     static lineCircle(sX, sY, eX, eY, cX, cY, r) {
 
+        /* Calculando colisão entre as duas extremidades e o círculo */
         let collided_p1 = this.pointCircle(sX, sY, cX, cY, r);
         let collided_p2 = this.pointCircle(eX, eY, cX, cY, r);
 
         if (collided_p1 || collided_p2) {
+
+            return true;
+        }
+
+        /* Calculando o ponto da reta mais próximo do círculo */
+        // calculando comprimento da linha através do teorema de Pitágoras.
+        let line_len = Physics.distance(sX, sY, eX, eY);
+
+        // calculando o produto escalar.
+        let dot = ( ((cX-sX)*(eX-sX)) + ((cY-sY)*(eY-sY)) ) / line_len**2;
+
+        // calculando ponto mais próximo.
+        let closestX = sX + (dot * (eX - sX));
+        let closestY = sY + (dot * (eY - sY));
+
+        // testando se o ponto mais próximo calculado pertence a reta.
+        let onSegment = this.linePoint(sX, sY, eX, eY, closestX, closestY);
+
+        if(!onSegment) {
+
+            return false;
+        }
+
+        // se o ponto mais próximo colidiu com o círculo.
+        let collided = this.pointCircle(closestX, closestY, cX, cY, r);
+
+        if(collided) {
 
             return true;
         }
